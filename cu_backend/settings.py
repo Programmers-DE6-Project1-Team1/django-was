9c07a70db48e2391355449334f75bc6eae94c1e9
dawit0905 <dawit0905@gmail.com>
2025-04-20 23:05:57 +0900

    feat: 요청을 받았을 때, json을 기본적으로 리턴하게 변경

--- cu_backend/settings.py
+++ cu_backend/settings.py
@@ -125,4 +125,7 @@ DEFAULT_AUTO_FIELD = 'django.db.models.BigAutoField'
 
 REST_FRAMEWORK = {
     'DEFAULT_FILTER_BACKENDS': ['django_filters.rest_framework.DjangoFilterBackend'],
+    'DEFAULT_RENDERER_CLASSES': [
+        'rest_framework.renderers.JSONRenderer',
+    ],
 }
