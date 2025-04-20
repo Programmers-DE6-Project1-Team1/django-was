c124115b86aff7331e88f17ac7f2f0e618447595
dawit0905 <dawit0905@gmail.com>
2025-04-20 22:57:51 +0900

    feat: add product filter

--- products/views.py
+++ products/views.py
@@ -1,3 +1,20 @@
-from django.shortcuts import render
+from rest_framework import generics
+from .models import Product
+from .serializers import ProductSerializer
+from django_filters.rest_framework import DjangoFilterBackend
+from django_filters import rest_framework as filters
 
-# Create your views here.
+class ProductFilter(filters.FilterSet):
+    price_min = filters.NumberFilter(field_name="price", lookup_expr='gte')
+    price_max = filters.NumberFilter(field_name="price", lookup_expr='lte')
+
+    class Meta:
+        model = Product
+        fields = ['price_min', 'price_max', 'label']
+
+
+class ProductListAPIView(generics.ListAPIView):
+    queryset = Product.objects.all()
+    serializer_class = ProductSerializer
+    filter_backends = [DjangoFilterBackend]
+    filterset_class = ProductFilter
