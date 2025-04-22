b92e4c7eb166e8a2b03a7b58dbc37a5a92cf0108
dawit <35230675+dawit0905@users.noreply.github.com>
2025-04-22 16:25:18 +0900

    feat: add all items api (#7)

--- products/urls.py
+++ products/urls.py
@@ -4,10 +4,12 @@ from .views import (
     TagListAPIView,
     LabelListAPIView,
     PromotionTagListAPIView,
+    ProductListAllAPIView,
 )
 
 urlpatterns = [
     path('api/products/', ProductListAPIView.as_view(), name='product-list'),
+    path("api/products/all/",  ProductListAllAPIView.as_view(),  name="product-list-all"),
     path('api/tags/', TagListAPIView.as_view(), name='tag-list'),
     path('api/labels/', LabelListAPIView.as_view(), name='label-list'),
     path('api/promotion-tags/', PromotionTagListAPIView.as_view(), name='promotiontag-list'),
