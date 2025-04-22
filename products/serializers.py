4f6f846ca9c17e64bc6315e0ea840760552fa4e0
dawit <35230675+dawit0905@users.noreply.github.com>
2025-04-22 09:28:03 +0900

    feature/add-category (#5)
    * feat: update import_csv
    
    * feat: update Product model
    - Many to Many 로 정규화
    
    * chore: update migration

--- products/serializers.py
+++ products/serializers.py
@@ -1,7 +1,23 @@
 from rest_framework import serializers
-from .models import Product
+from .models import Product, Tag, PromotionTag, Label
 
 class ProductSerializer(serializers.ModelSerializer):
+    tags = serializers.SlugRelatedField(
+        many=True,
+        read_only=True,
+        slug_field='name'
+    )
+    promotion_tags = serializers.SlugRelatedField(
+        many=True,
+        read_only=True,
+        slug_field='name'
+    )
+    labels = serializers.SlugRelatedField(
+        many=True,
+        read_only=True,
+        slug_field='name'
+    )
+
     class Meta:
         model = Product
         fields = '__all__'
